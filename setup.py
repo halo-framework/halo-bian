19ccdc18335c369f01bd68b5a21e12cdb61a0550
yoramk <yoramk2@yahoo.com>
2019-02-10 00:05:22 +0200

    new

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='halo_bian',
-    version='0.11.5',
+    version='0.11.6',
     packages=['halo_bian', 'halo_bian.bian'],
     url='https://github.com/yoramk2/halo_bian',
     license='MIT License',
