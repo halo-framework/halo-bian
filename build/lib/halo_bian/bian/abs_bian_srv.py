aa7be30ec382fdbfde0c8add5fc75a93fd7bd199
yoramk <yoramk2@yahoo.com>
2019-03-06 19:05:39 +0200

    new

---
+++ build/lib/halo_bian/bian/abs_bian_srv.py
@@ -0,0 +1,384 @@
+#!/usr/bin/env python
+import json
+
+from halo_flask.exceptions import ApiError
+from halo_flask.flask.mixinx import AbsBaseMixinX as AbsBaseMixin
+from halo_flask.flask.utilx import Util
+from halo_flask.flask.utilx import status
+from halo_flask import settingsx
+
+from halo_bian.bian.exceptions import *
+from halo_bian.bian.bian import *
+
+settings = settingsx()
+
+logger = logging.getLogger(__name__)
+
+
+class AbsBianMixin(AbsBaseMixin):
+    __metaclass__ = ABCMeta
+
+    service_domain = None
+    service_properties = None
+    service_status = None
+    control_record = None
+    functional_pattern = None
+    bian_service_info = None
+    bian_action = None
+    service_operations = ServiceOperations
+
+    def __init__(self):
+        super(AbsBaseMixin, self).__init__()
+        if settings.SERVICE_DOMAIN:
+            self.service_domain = settings.SERVICE_DOMAIN
+        else:
+            raise ServiceDomainNameException("missing Service Domain definition")
+        if settings.FUNCTIONAL_PATTERN:
+            self.functional_pattern = settings.FUNCTIONAL_PATTERN
+        else:
+            raise FunctionalPatternNameException("missing Functional Pattern definition")
+        if self.functional_pattern not in FunctionalPatterns.patterns.keys():
+            raise FunctionalPatternNameException("Functional Pattern name not in list")
+        self.bian_service_info = BianServiceInfo(self.service_domain, self.functional_pattern,
+                                                 self.get_control_record())
+        self.set_action()
+
+    def set_action(self):
+        pass
+
+    def set_control_record(self, control_record):
+        self.control_record = control_record
+        self.bian_service_info = BianServiceInfo(self.get_service_domain(), self.get_functional_pattern(),
+                                                 self.get_control_record())
+
+    def get_control_record(self):
+        return self.control_record
+
+    def init_bq(self, bq_class_name):
+        import importlib
+        module = importlib.import_module("halo_bian.bian.bian")
+        class_ = getattr(module, bq_class_name)
+        instance = class_(settings.BEHAVIOR_QUALIFIER)
+        return instance
+
+    def get_behavior_qualifier(self, op, bq):
+        bq_class = FunctionalPatterns.patterns[self.functional_pattern][1]
+        bq_obj = self.init_bq(bq_class)
+        for bq_id in bq_obj.keys():
+            bq_str = bq_obj.get(bq_id)
+            if bq_str.lower() == bq.lower():
+                return bq_str.strip().replace("-","_").replace(" ","_")
+        raise IllegalBQException(bq)
+
+    def get_behavior_qualifier_by_id(self, op, bq_id):
+        bq_class = FunctionalPatterns.patterns[self.functional_pattern][1]
+        bq_obj = self.init_bq(bq_class)
+        if bq_id in bq_obj.keys():
+            bq_str = bq_obj.get(bq_id)
+            if bq_str:
+                return bq_str.strip().replace("-","_").replace(" ","_")
+        raise IllegalBQIdException(bq_id)
+
+    def get_collection_filter(self, collection_filter):
+        ret = None
+        arr = []
+        if collection_filter is not None:
+            if ";" in collection_filter:
+                arr = collection_filter.split(";")
+            else:
+                arr.append(collection_filter)
+            ret = arr
+        return ret
+
+    def bian_validate_req(self, action, request, vars):
+        logger.debug("in bian_validate_req " + str(action) + " vars=" + str(vars))
+        service_op = action.upper()
+        if service_op not in ServiceOperations.ops:
+            raise IllegalServiceOperationException(action)
+        cr_reference_id = None
+        behavior_qualifier = None
+        bq_reference_id = None
+        collection_filter = None
+        if "cr_reference_id" in vars:
+            cr_reference_id = vars["cr_reference_id"]
+        if "behavior_qualifier" in vars:
+            behavior_qualifier = self.get_behavior_qualifier(service_op, vars["behavior_qualifier"])
+        if "bq_reference_id" in vars:
+            bq_reference_id = vars["bq_reference_id"]
+            #behavior_qualifier = self.get_behavior_qualifier_by_id(service_op, vars["bq_reference_id"])
+        if "collection-filter" in request.args:
+            collection_filter = self.get_collection_filter(request.args["collection-filter"])
+        return BianRequest(service_op, request, cr_reference_id=cr_reference_id, bq_reference_id=bq_reference_id, behavior_qualifier=behavior_qualifier,collection_filter=collection_filter)
+
+    # raise BianException()
+
+    def validate_req(self, bian_request):
+        logger.debug("in validate_req ")
+        if bian_request:
+            return True
+        raise BianException()
+
+    def set_back_api(self,bian_request):
+        logger.debug("in set_back_api ")
+        return None
+
+    def set_api_headers(self, bian_request):
+        logger.debug("in set_api_headers ")
+        if bian_request:
+            return []
+        raise BianException()
+
+    def set_api_vars(self, bian_request):
+        logger.debug("in set_api_vars " + str(bian_request))
+        if True:
+            ret = {}
+            ret["bq"] = bian_request.behavior_qualifier
+            ret["id"] = bian_request.cr_reference_id
+            return ret
+        raise BianException()
+
+    def set_api_auth(self, bian_request):
+        return None
+
+    def execute_api(self, bian_request, back_api, back_vars, back_headers,back_auth):
+        logger.debug("in execute_api ")
+        if back_api:
+            timeout = Util.get_timeout(bian_request.request)
+            try:
+                ret = back_api.get(timeout)
+                return ret
+            except ApiError as e:
+                raise BianException(e)
+        return None
+
+    def extract_json(self, bian_request, back_response):
+        logger.debug("in extract_json ")
+        if back_response:
+            return json.loads(back_response.content)
+        return json.loads("{}")
+
+    def create_resp_payload(self, bian_request, back_json):
+        logger.debug("in create_resp_payload " + str(back_json))
+        if back_json:
+            return back_json
+        return back_json
+
+    # raise BianException()
+    def set_resp_headers(self, bian_request, headers):
+        logger.debug("in set_resp_headers " + str(headers))
+        if headers:
+            return []
+        raise BianException()
+
+    def process_ok(self, response):
+        if response:
+            if response.bian_request:
+                if response.bian_request.request:
+                    if response.bian_request.request.method == 'get':
+                        response.code = status.HTTP_200_OK
+                    if response.bian_request.request.method == 'post':
+                        response.code = status.HTTP_201_CREATED
+                    if response.bian_request.request.method == 'put':
+                        response.code = status.HTTP_202_ACCEPTED
+                    if response.bian_request.request.method == 'patch':
+                        response.code = status.HTTP_202_ACCEPTED
+                    if response.bian_request.request.method == 'delete':
+                        response.code = status.HTTP_200_OK
+                    return response
+                raise ServiceOperationFailException(response.bian_request.service_operation)
+        raise ServiceOperationFailException(response)
+
+    def process_service_operation(self, action, request, vars):
+        logger.debug("in process_service_operation " + str(vars))
+        bian_request = self.bian_validate_req(action, request, vars)
+        functionName = {
+            ServiceOperations.INITIATE: self.do_initiate,
+            ServiceOperations.CREATE: self.do_create,
+            ServiceOperations.ACTIVATE: self.do_activate,
+            ServiceOperations.CONFIGURE: self.do_configure,
+            ServiceOperations.UPDATE: self.do_update,
+            ServiceOperations.REGISTER: self.do_register,
+            ServiceOperations.RECORD: self.do_record,
+            ServiceOperations.EXECUTE: self.do_execute,
+            ServiceOperations.EVALUATE: self.do_evaluate,
+            ServiceOperations.PROVIDE: self.do_provide,
+            ServiceOperations.AUTHORIZE: self.do_authorize,
+            ServiceOperations.REQUEST: self.do_request,
+            ServiceOperations.TERMINATE: self.do_terminate,
+            ServiceOperations.NOTIFY: self.do_notify,
+            ServiceOperations.RETRIEVE: self.do_retrieve
+        }[bian_request.service_operation]
+        if bian_request.service_operation in FunctionalPatterns.operations[self.functional_pattern]:
+            bian_response = functionName(bian_request)
+            return self.process_ok(bian_response)
+        raise IllegalServiceOperationException(bian_request.service_operation)
+
+    def do_initiate(self, bian_request):
+        logger.debug("in do_initiate ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_initiate_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_create(self, bian_request):
+        logger.debug("in do_create ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_create_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_activate(self, bian_request):
+        logger.debug("in do_activate ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_activate_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_configure(self, bian_request):
+        logger.debug("in do_configure ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_configure_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_update(self, bian_request):
+        logger.debug("in do_update ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_update_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_register(self, bian_request):
+        logger.debug("in do_register ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_register_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_record(self, bian_request):
+        logger.debug("in do_record ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_record_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_execute(self, bian_request):
+        logger.debug("in do_execute ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_execute_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_evaluate(self, bian_request):
+        logger.debug("in do_evaluate ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_evaluate_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_provide(self, bian_request):
+        logger.debug("in do_provide ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_provide_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_authorize(self, bian_request):
+        logger.debug("in do_authorize ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_authorize_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_request(self, bian_request):
+        logger.debug("in do_request ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_request_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_terminate(self, bian_request):
+        logger.debug("in do_terminate ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_terminate_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_notify(self, bian_request):
+        logger.debug("in do_notify ")
+        if bian_request.behavior_qualifier:
+            return getattr(self, 'do_notify_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+
+    def do_retrieve_bq(self, bian_request):
+        logger.debug("in do_retrieve_bq ")
+        if bian_request.behavior_qualifier is None:
+            raise IllegalBQException("missing behavior_qualifier value")
+        try:
+            behavior_qualifier = bian_request.behavior_qualifier.lower()
+            # 1. validate in params
+            getattr(self, 'validate_req_%s' % behavior_qualifier)(bian_request)
+            # 2. Code to access the BANK API  to retrieve data - url + vars dict
+            back_api = getattr(self, 'set_back_api_%s' % behavior_qualifier)(bian_request)
+            # 3. array to store the headers required for the API Access
+            back_headers = getattr(self, 'set_api_headers_%s' % behavior_qualifier)(bian_request)
+            # 4. Sending the request to the BANK API with params
+            back_vars = getattr(self, 'set_api_vars_%s' % behavior_qualifier)(bian_request)
+            back_auth = getattr(self, 'set_api_auth_%s' % behavior_qualifier)(bian_request)
+            back_response = getattr(self, 'execute_api_%s' % behavior_qualifier)(bian_request, back_api, back_vars, back_headers, back_auth)
+            # 5. extract from Response stored in an object built as per the BANK API Response body JSON Structure
+            back_json = getattr(self, 'extract_json_%s' % behavior_qualifier)(bian_request,back_response)
+            # 6. Build the payload target response structure which is IFX Compliant
+            payload = getattr(self, 'create_resp_payload_%s' % behavior_qualifier)(bian_request,back_json)
+            logger.debug("payload=" + str(payload))
+            headers = getattr(self, 'set_resp_headers_%s' % behavior_qualifier)(bian_request,bian_request.request.headers)
+            # 7. build json and add to bian response
+            ret = BianResponse(bian_request, payload, headers)
+            # return json response
+            return ret
+        except AttributeError as ex:
+            raise BianMethodNotImplementedException(ex)
+
+
+    def do_retrieve(self, bian_request):
+        logger.debug("in do_retrieve ")
+        if bian_request.behavior_qualifier:
+            try:
+                return getattr(self, 'do_retrieve_%s' % bian_request.behavior_qualifier.lower())(bian_request)
+            except AttributeError as ex:
+                raise BianMethodNotImplementedException(ex)
+        # 1. validate in params
+        self.validate_req(bian_request)
+        # 2. Code to access the BANK API  to retrieve data - url + vars dict
+        back_api = self.set_back_api(bian_request)
+        # 3. array to store the headers required for the API Access
+        back_headers = self.set_api_headers(bian_request)
+        # 4. Sending the request to the BANK API with params
+        back_vars = self.set_api_vars(bian_request)
+        back_auth = self.set_api_auth(bian_request)
+        back_response = self.execute_api(bian_request, back_api, back_vars, back_headers, back_auth)
+        # 5. extract from Response stored in an object built as per the BANK API Response body JSON Structure
+        back_json = self.extract_json(bian_request,back_response)
+        # 6. Build the payload target response structure which is IFX Compliant
+        payload = self.create_resp_payload(bian_request,back_json)
+        logger.debug("payload=" + str(payload))
+        headers = self.set_resp_headers(bian_request,bian_request.request.headers)
+        # 7. build json and add to bian response
+        ret = BianResponse(bian_request, payload, headers)
+        # return json response
+        return ret
+
+    def get_service_domain(self):
+        return self.service_domain
+
+    def get_functional_pattern(self):
+        return self.functional_pattern
+
+    def get_bian_info(self):
+        return self.bian_service_info
+
+    def get_service_status(self):
+        return self.service_status
+
+    def get_action(self,default):
+        if self.bian_action:
+            return self.bian_action
+        return default
+
+    def process_get(self, request, vars):
+        logger.debug("sd=" + str(self.service_domain) + " in process_get " + str(vars))
+        action = self.get_action("retrieve")
+        return self.process_service_operation(action, request, vars)
+
+    def process_post(self, request, vars):
+        logger.debug("in process_post " + str(vars))
+        action = self.get_action("create")
+        return self.process_service_operation(action, request, vars)
+
+    def process_put(self, request, vars):
+        logger.debug("in process_put " + str(vars))
+        action = self.get_action("update")
+        return self.process_service_operation(action, request, vars)
+
+    def process_patch(self, request, vars):
+        logger.debug("in process_patch " + str(vars))
+        action = self.get_action("update")
+        return self.process_service_operation(action, request, vars)
+
+    def process_delete(self, request, vars):
+        logger.debug("in process_delete " + str(vars))
+        action = self.get_action("terminate")
+        return self.process_service_operation(action, request, vars)
---
+++ build/lib/halo_bian/bian/exceptions.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from halo_flask.exceptions import HaloException
+from abc import ABCMeta
+
+class BianException(HaloException):
+    pass
+
+
+class IllegalServiceOperationException(BianException):
+    pass
+
+
+class ServiceOperationFailException(BianException):
+    pass
+
+
+class IllegalBQException(BianException):
+    pass
+
+class IllegalBQIdException(BianException):
+    pass
+
+class BianMethodNotImplementedException(BianException):
+    pass
+
+class ServiceDomainNameException(BianException):
+    pass
+
+class FunctionalPatternNameException(BianException):
+    pass
+
+class BianApiException(BianException):
+    pass
