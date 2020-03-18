5cbfa2bd307f80093835a1f4e7da6564c3215650
yoramk2 <yoramk2@yahoo.com>
2020-03-18 23:08:40 +0200

    fux

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='halo_bian',
-    version='0.11.51',
+    version='0.11.52',
     packages=['halo_bian', 'halo_bian.bian','docs'],
     url='https://github.com/yoramk2/halo_bian',
     license='MIT License',
