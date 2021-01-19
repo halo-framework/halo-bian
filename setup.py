781979e8ffe4bd6761a777e0b0d00a3a9e640901
halo-framework <halo-framework@gmail.com>
2021-01-20 01:06:30 +0200

    init

--- setup.py
+++ setup.py
@@ -12,8 +12,9 @@ with open("README.md", "r") as fh:
 
 setup(
     name='halo-bian',
-    version='0.12.19',
-    packages=['halo_bian', 'halo_bian.bian','halo_bian.bian.plugins','halo_bian.bian.app','halo_bian.bian.domain','docs'],
+    version='0.12.20',
+    packages=['halo_bian', 'halo_bian.bian','halo_bian.bian.plugins','halo_bian.bian.app',
+              'halo_bian.bian.domain','halo_bian.bian.view','docs'],
     url='https://github.com/halo-framework/halo-bian',
     license='MIT License',
     author='halo-framework',
